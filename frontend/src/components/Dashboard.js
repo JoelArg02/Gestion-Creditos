6ab531e1cd0634c841ce20b3b9d3a9860c56b23b
Joel Daniel Arguello Espinosa <jdarguello1@espe.edu.ec>
2023-12-17 01:58:06 -0500

    a

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Table, Alert, Button, Modal, Form } from "react-bootstrap";
-import { getCredits } from "../api/api";  
-import './Dashboard.css';
-
+import { getCredits } from "../api/api";
+import "./Dashboard.css"; // Import your external CSS file
 
 function AddCreditModal({ show, handleClose, handleSubmit }) {
   return (
@@ -11,9 +10,7 @@ function AddCreditModal({ show, handleClose, handleSubmit }) {
         <Modal.Title>Añadir Crédito</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <Form onSubmit={handleSubmit}>
-          {/* Agrega campos de formulario según tu modelo de datos */}
-        </Form>
+        <Form onSubmit={handleSubmit}>{/* Add form fields here */}</Form>
       </Modal.Body>
       <Modal.Footer>
         <Button variant="secondary" onClick={handleClose}>
@@ -49,45 +46,23 @@ function Dashboard() {
   };
 
   return (
-
-    <div>
+    <div className="dashboard-container text-center"> {/* Add "text-center" class */}
       <h1>Dashboard</h1>
       <Button variant="primary" onClick={() => setShowModal(true)}>
         Añadir Crédito
       </Button>
-      <AddCreditModal show={showModal} handleClose={() => setShowModal(false)} handleSubmit={handleAddCredit} />
+      <AddCreditModal
+        show={showModal}
+        handleClose={() => setShowModal(false)}
+        handleSubmit={handleAddCredit}
+      />
       {error ? (
         <Alert variant="danger">Error en el servidor: {error.message}</Alert>
       ) : credits.length === 0 ? (
         <Alert variant="info">No hay información de créditos disponible.</Alert>
       ) : (
         <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>ID Crédito</th>
-              <th>ID Usuario Crea</th>
-              <th>ID Usuario Revisa</th>
-              <th>Monto</th>
-              <th>Interés</th>
-              <th>Fecha Inicio</th>
-              <th>Fecha Vencimiento</th>
-              <th>Estado</th>
-            </tr>
-          </thead>
-          <tbody>
-            {credits.map((credit) => (
-              <tr key={credit.id_credito}>
-                <td>{credit.id_credito}</td>
-                <td>{credit.id_usuario_credito_crea}</td>
-                <td>{credit.id_usuario_credito_revisa}</td>
-                <td>{credit.monto}</td>
-                <td>{credit.interes}</td>
-                <td>{credit.fecha_inicio}</td>
-                <td>{credit.FechaVencimiento}</td>
-                <td>{credit.Estado ? "Activo" : "Inactivo"}</td>
-              </tr>
-            ))}
-          </tbody>
+          {/* Table content here */}
         </Table>
       )}
     </div>
