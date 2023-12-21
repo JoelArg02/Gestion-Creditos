0f621e75550a38fef78d388be0664b255b7cd687
Joel Daniel Arguello Espinosa <jdarguello1@espe.edu.ec>
2023-12-21 09:40:07 -0500

    update

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="bg-dark text-white text-center p-3">
       &copy; Servicio de Gestión de Créditos en la Nube. Todos los derechos
-        reservados.
+        reservados                                    
     </footer>
   );
 }
